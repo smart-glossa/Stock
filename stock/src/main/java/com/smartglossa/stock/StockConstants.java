091e37b8239fa3a252a245dc2467178f9362c254
SathishSmart <sathishprs@smartglossa.com>
2017-01-10 22:36:20 +0530

    Delete StockConstants.java

--- stock/src/main/java/com/smartglossa/stock/StockConstants.java
+++
@@ -1,9 +0,0 @@
-package com.smartglossa.stock;
-
-public class StockConstants {
-	 public final static String MYSQL_SERVER =System.getProperty("stock.mysql");
-	 public final static String USERNAME =System.getProperty("stock.username");
-	 public final static String PASSWORD =System.getProperty("stock.password");
-	 public final static String DATABASE =System.getProperty("stock.database");
-
-}
