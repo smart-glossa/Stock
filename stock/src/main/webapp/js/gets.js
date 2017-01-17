f66f3fbe3c71fba05475006b88455cf07889237f
Sathish <sathishprs@smartglossa>
2017-01-17 12:46:25 +0530

    change js file

--- stock/src/main/webapp/js/gets.js
+++ stock/src/main/webapp/js/gets.js
@@ -10,7 +10,7 @@ function displaybill() {
 			var query = "<select>"
 					if(result!="undefined"){
 				for (var i = 0; i < array.length; i++) {
-					query += "<option class="+ array[i].prodid +">" + array[i].prodname+ "</option>";
+					query += "<option class='ss',"+ array[i].prodid +">" + array[i].prodname+ "</option>";
 					//query += "<option>" + array[i].prodname+ "</option>";
 					
 									}
@@ -23,3 +23,22 @@ function displaybill() {
 		});
 
 	}
+$(document).ready(function() {
+	 $(document).on("click", ".ss", function() {
+			// If user not confirmed, then dont execute, just return back.
+		// if (!confirm(" Delete Are you sure?")) {
+		  // return;
+	//	}
+		var tag = $(this).parent().parent();
+		var billno = tag.children(".parid")[0].innerHTML;
+		var url = "/arun/bill?operation=delete&abillno=" + billno;
+		$.ajax({
+		    url: url,
+		    type: 'POST'
+		}).done(function(result) {
+		   tag.remove();
+		}).fail(function(result) {
+		    console.log(result)
+		});
+		});
+})
