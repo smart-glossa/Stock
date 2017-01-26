20a12859794ab3a7a06c234165dc9628ce684816
Sathish <sathishprs@smartglossa>
2017-01-26 13:19:43 +0530

    change css and js file

--- stock/src/main/webapp/js/gets.js
+++ stock/src/main/webapp/js/gets.js
@@ -16,33 +16,8 @@ function displaybill() {
 				}
 			}
 			query += "</select>"
-				$(document).on("click", ".ss", function() {
-					var prodid = $(this).val();
-					var url = "/stock/Product?operation=gets&prodid=" + prodid;
-					$.ajax({
-						url: url,
-						type: 'POST'
-					}).done(function(result) {
-						
-						var array = JSON.parse(result);
-						var query = "<select class='sele2'>"
-						query += "<tr><th>ParentProductName</th></tr>"
-						if(result!="undefined"){
-							for (var i = 0; i < array.length; i++) {
-								query += "<option>" + array[i].prodname + "</option>";
-								
-							}
-						}
-						query += "</select>"
-							query += "<input type=\"text\" id=\"prodname\" placeholder=\"ParentProductName\" class=\"pname\">";
-							query +=  "<input type=\"submit\" id=\"parent\" class=\"psub\">";
-							//$(".adds")[0].innerHTML =query;
-
-						}).fail(function(result) {
-							console.log(result)
-						//});
-					});
-			})
+				query += "<input type=\"text\" id=\"prodname\" placeholder=\"ParentProductName\" class=\"pname\">";
+			query +=  "<input type=\"submit\" id=\"parent\" class=\"psub\">";
 			$(".add")[0].innerHTML =query;
 
 		}).fail(function() {
@@ -50,7 +25,7 @@ function displaybill() {
 		});
 
 	}
-	/*$(document).ready(function() {
+	$(document).ready(function() {
 		$(document).on("click", ".ss", function() {
 			var prodid = $(this).val();
 			var url = "/stock/Product?operation=gets&prodid=" + prodid;
@@ -69,12 +44,12 @@ function displaybill() {
 					}
 				}
 				query += "</select>"
-					query += "<input type=\"text\" id=\"prodname\" placeholder=\"ParentProductName\" class=\"pname\">";
-					query +=  "<input type=\"submit\" id=\"parent\" class=\"psub\">";
+				//	query += "<input type=\"text\" id=\"prodname\" placeholder=\"ParentProductName\" class=\"pname\">";
+					//query +=  "<input type=\"submit\" id=\"parent\" class=\"psub\">";
 					$(".adds")[0].innerHTML =query;
 
 				}).fail(function(result) {
 					console.log(result)
-				//});
+				});
 			});
-	})*/
+	})
