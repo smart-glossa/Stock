49d26aee7af26a1eb3d56c01dad6076870e4fd33
Sathish <sathishprs@smartglossa>
2017-01-26 12:13:07 +0530

    correction js file

--- stock/src/main/webapp/js/gets.js
+++ stock/src/main/webapp/js/gets.js
@@ -16,6 +16,33 @@ function displaybill() {
 				}
 			}
 			query += "</select>"
+				$(document).on("click", ".ss", function() {
+					var prodid = $(this).val();
+					var url = "/stock/Product?operation=gets&prodid=" + prodid;
+					$.ajax({
+						url: url,
+						type: 'POST'
+					}).done(function(result) {
+						
+						var array = JSON.parse(result);
+						var query = "<select class='sele2'>"
+						query += "<tr><th>ParentProductName</th></tr>"
+						if(result!="undefined"){
+							for (var i = 0; i < array.length; i++) {
+								query += "<option>" + array[i].prodname + "</option>";
+								
+							}
+						}
+						query += "</select>"
+							query += "<input type=\"text\" id=\"prodname\" placeholder=\"ParentProductName\" class=\"pname\">";
+							query +=  "<input type=\"submit\" id=\"parent\" class=\"psub\">";
+							//$(".adds")[0].innerHTML =query;
+
+						}).fail(function(result) {
+							console.log(result)
+						//});
+					});
+			})
 			$(".add")[0].innerHTML =query;
 
 		}).fail(function() {
@@ -23,7 +50,7 @@ function displaybill() {
 		});
 
 	}
-	//$(document).ready(function() {
+	/*$(document).ready(function() {
 		$(document).on("click", ".ss", function() {
 			var prodid = $(this).val();
 			var url = "/stock/Product?operation=gets&prodid=" + prodid;
@@ -50,4 +77,4 @@ function displaybill() {
 					console.log(result)
 				//});
 			});
-	})
+	})*/
